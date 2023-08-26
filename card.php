f900677ef29ed1f14db7a6ed9abe07b97f6e3fe7
ariful171042 <arif171042@gmail.com>
2023-08-26 16:09:17 +0600

    frist commit

---
+++ card.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+      crossorigin="anonymous"
+    ></script>
+    <script defer src="./assets/script.js"></script>
+  </head>
+  <body>
+
+    <div class="container mt-5">
+      <div class="row gap-5">
+        <!-- card start  -->
+        <div class="card p-0" style="width: 18rem">
+          <img
+            src="https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            class="card-img-top img-fluid w-100"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!--card end-->
+        <!-- card start  -->
+        <div class="card p-0" style="width: 18rem">
+          <img
+            src="https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            class="card-img-top img-fluid w-100"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!--card end-->
+        <!-- card start  -->
+        <div class="card p-0" style="width: 18rem">
+          <img
+            src="https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            class="card-img-top img-fluid w-100"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!--card end-->
+        <!-- card start  -->
+        <div class="card p-0" style="width: 18rem">
+          <img
+            src="https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            class="card-img-top img-fluid w-100"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!--card end-->
+      </div>
+    </div>
+  </body>
+</html>
---
+++ tranding.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <div class=" mb-5">
+    <div class="container mt-5 mb-5">
+      <div class="">
+        <div class="row justify-content-between">
+          <!-- card start -->
+          <a href="" class="col-2 text-decoration-none">
+            <img
+              src="https://images.pexels.com/photos/3587478/pexels-photo-3587478.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+              class="w-100 h-50 object-fit-cover"
+            />
+            <p class="text-center fw-bold fs-4">Head Phone</p>
+          </a>
+          <!-- card end -->
+          <!-- card start -->
+          <a href="#" class="col-2 text-decoration-none">
+            <img
+              src="https://images.pexels.com/photos/18015765/pexels-photo-18015765/free-photo-of-hand-holding-camera.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+              class="w-100 h-75 object-fit-cover"
+            />
+            <p class="text-center fw-bold fs-4">Camera</p>
+          </a>
+          <!-- card end -->
+          <!-- card start -->
+          <a href="#" class="col-3 text-decoration-none">
+            <img
+              src="https://images.pexels.com/photos/2528118/pexels-photo-2528118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              class="w-100 h-100 object-fit-cover"
+            />
+            <p class="text-center fw-bold fs-4">Computer</p>
+          </a>
+          <!-- card end -->
+          <!-- card start -->
+          <a href="#" class="col-2 text-decoration-none">
+            <img
+              src="https://images.pexels.com/photos/4740522/pexels-photo-4740522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              class="w-100 h-75 object-fit-cover"
+            />
+            <p class="text-center fw-bold fs-4">Television</p>
+          </a>
+          <!-- card end -->
+          <!-- card start -->
+          <a class="col-2 text-decoration-none">
+            <img
+              src="https://images.pexels.com/photos/4526469/pexels-photo-4526469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              class="w-100 h-50 object-fit-cover"
+            />
+            <p class="text-center fw-bold fs-4">Phone</p>
+          </a>
+          <!-- card end -->
+        </div>
+      </div>
+    </div>
+    </div>
+  </body>
+</html>
